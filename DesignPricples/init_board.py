d38a4a09c9c3c03aab0fcb69b232c5b4bce1d5a0
SewarMahagna <sewarma.17@gmail.com>
2024-12-05 21:47:30 +0200

    file init_board.py updated

---
+++ DesignPricples/init_board.py
@@ -0,0 +1,18 @@
+import random
+
+def initialize_board(size, elements): 
+    if size <= 0:
+        raise ValueError("Size must be a positive integer.")
+    if len(elements) < size * size:
+        raise ValueError("Not enough elements to fill the board.")
+
+    random.shuffle(elements)
+    board = [elements[i * size:(i + 1) * size] for i in range(size)]
+    return board
+
+
+size = 4  
+elements = list(range(1, (size * size) + 1))
+board = initialize_board(size, elements)
+for row in board:
+    print(row)
