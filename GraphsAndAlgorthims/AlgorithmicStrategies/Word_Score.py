9c4c2f3e2fd7d37367c9eb0e15745d3c5d6b4b9e
SewarMahagna <sewarma.17@gmail.com>
2024-12-15 20:34:39 +0200

    modified:   GraphsAndAlgorthims/AlgorithmicStrategies/Word_Score.py

--- GraphsAndAlgorthims/AlgorithmicStrategies/Word_Score.py
+++ GraphsAndAlgorthims/AlgorithmicStrategies/Word_Score.py
@@ -3,18 +3,29 @@ def char_position(char):
         return ord(char.lower()) - ord('a') + 1
     return 0
 
+
 def Word_Score(given_word): 
     if not given_word:
         return 0
-    return sum(char_position(char) for char in given_word)
+    score = 0 
+    for char in given_word : 
+        score +=char_position(char) 
+    return score
 
 def Str_Score(given_str): 
     words_lst = given_str.split()
-    max_word = max(words_lst, key=Word_Score)
-    print("The word with max score is : ", max_word)
+    highest_score = Word_Score(words_lst[0])
+    max_word = words_lst[0]
+    for word in words_lst :
+        new_score = Word_Score(word)
+        if new_score > highest_score : 
+            highest_score = new_score
+            max_word = word
+    print(max_word)
 
 
-str = "Hello from python "
+
+str = "Happy Birthday"
 
 Str_Score(str)
 
