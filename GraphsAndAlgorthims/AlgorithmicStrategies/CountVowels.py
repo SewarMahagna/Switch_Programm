651a58a2cef3bede0a901eeedb8db906ed555bc7
SewarMahagna <sewarma.17@gmail.com>
2024-12-16 09:21:38 +0200

    modified:   GraphsAndAlgorthims/AlgorithmicStrategies/CountVowels.py 	modified:   GraphsAndAlgorthims/AlgorithmicStrategies/Median_Char.py

--- GraphsAndAlgorthims/AlgorithmicStrategies/CountVowels.py
+++ GraphsAndAlgorthims/AlgorithmicStrategies/CountVowels.py
@@ -1,11 +1,15 @@
 
-vowels = ["A","a","E","e","I","i","O","o","U","u"]
+vowels = {"A","a","E","e","I","i","O","o","U","u"}
 def count_vowels(given_str): 
-    counter = 0 
+    vowels_counter = 0 
+    const_count= 0
     for char in given_str : 
-        if char in vowels:
-            counter+=1 
-    print(f"The Number of vowels in - {given_str} is : {counter}")
+        if char.isalpha(): 
+            if char in vowels:
+                vowels_counter+=1
+            else: 
+                const_count += 1 
+    print(f"The Number of vowels in - {given_str} is : {vowels_counter}")
 
 str1= "Hello My Name Is Sewar"
 str2 = "Vowel"
